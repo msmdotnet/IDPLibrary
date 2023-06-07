7da6d0c2e32f300d074616eb82863b51592e08e8
Miguel Muñoz Serafín <mmunoz@dotnetpuebla.com>
2023-06-07 14:53:26 -0600

    Add unit test example for IClientProvider

---
+++ src/IDPLibrary/IDPLibrary.MemoryClientProvider/ClientProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace IDPLibrary.MemoryClientProvider;
+internal class ClientProvider : IClientProvider
+{
+    readonly IReadOnlyList<Client> Clients;
+    public ClientProvider(IOptions<MemoryClientListOptions> options)
+    {
+        Clients = new List<Client>(options.Value.Clients);
+    }
+    public Task<Client> GetClientAsync(string clientId, string redirectUri) =>
+
+        clientId == "client1" && redirectUri == "https://localhost/callback" ?
+        Task.FromResult(new Client { ClientId = clientId, RedirectUris = new string[] { redirectUri } }) :
+        Task.FromResult<Client>(null);
+}
