47a359281db5bc3e36c40dbae281f1372f4f29ea
Sergi Ortiz Gómez <drualcman@msn.com>
2023-06-09 05:24:18 +0800

    Eliminacion de proyecto desechado. Se me olvido.

--- src/IDPLibrary/IDPLibrary.Clients/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using IDPLibrary.Clients.Options;
-global using IDPLibrary.Clients.Specifications;
-global using IDPLibrary.Entities.Entities;
-global using IDPLibrary.Entities.Interfaces;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Options;
--- src/IDPLibrary/IDPLibrary.Clients/Options/ClientList.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace IDPLibrary.Clients.Options;
-internal class ClientList
-{
-    public const string SectionKey = "Clients";
-    public Client[] Clients { get; set; }
-}
